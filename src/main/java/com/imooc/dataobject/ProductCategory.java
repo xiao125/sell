88c586a85af2bc50cb5e79a34c33f1a89d9c13ae
xiao125 <1252241048@qq.com>
2018-04-05 10:37:02 +0800

    DaoTest

---
+++ src/main/java/com/imooc/dataobject/ProductCategory.java
@@ -0,0 +1,44 @@
+package com.imooc.dataobject;
+
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 类目
+ */
+
+@Entity
+@DynamicUpdate  //更新某个字段时,更新所有的字段,
+@Data  //自动生成set,get, tostring 方法  pom需要引入lombok
+public class ProductCategory {
+
+    /**
+     * 类目id
+     */
+    @Id
+    @GeneratedValue
+    private Integer categoryId;
+
+    /**
+     * 类目名字
+     */
+    private String categoryName;
+
+    /**
+     * 类目编号.
+     */
+    private Integer categoryType;
+
+    public ProductCategory() {
+    }
+
+    public ProductCategory(String categoryName, Integer categoryType) {
+        this.categoryName = categoryName;
+        this.categoryType = categoryType;
+    }
+}
---
+++ src/test/java/com/imooc/ProductCategoryRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.imooc;
+
+import com.imooc.dataobject.ProductCategory;
+import com.imooc.repository.ProductCategoryRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCategoryRepositoryTest {
+
+
+    @Autowired
+    private ProductCategoryRepository repository;
+
+
+    @Test
+    public void findOneTest(){
+
+        ProductCategory productCategory = repository.findOne(1);
+        System.out.println(productCategory.toString());
+
+    }
+
+    @Test
+    @Transactional
+    public void saveTest(){
+
+        ProductCategory productCategory = new ProductCategory("男生最爱",4);
+        ProductCategory result = repository.save(productCategory);
+        Assert.assertNotNull(result);
+
+    }
+
+    @Test
+    public void findByCategoryTypeInTest(){
+
+        List<Integer> list = Arrays.asList(2,3,4);
+        List<ProductCategory> result = repository.findByCategoryTypeIn(list);
+        Assert.assertNotEquals(0,result.size());
+    }
+
+
+
+
+
+
+}
